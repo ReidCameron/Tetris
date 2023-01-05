ea3c8a6d8e6b65781da1ebbff268c948b8a4c5cb
Cameron Reid <reidcj@reidcj-5520.rose-hulman.edu>
2023-01-05 11:39:41 -0600

    changed default file to index.ejs

--- express/server.js
+++ express/server.js
@@ -11,7 +11,7 @@ app.use('/',(req, res)=>{
 
 //debug
 app.get("/*", (req, res) =>{
-  res.sendFile(path.join(__dirname, "../public/index.ejs"))
+  res.sendFile(path.join(__dirname, "../views/index.ejs"))
 })
 
 //serverless
