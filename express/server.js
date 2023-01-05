c157c0652132635411437dbf23a9be3b3a1c80d9
Cameron Reid <reidcj@reidcj-5520.rose-hulman.edu>
2023-01-05 12:23:10 -0600

    added function to create index during netlify build

--- express/server.js
+++ express/server.js
@@ -1,37 +1,18 @@
 const express = require ('express');
-const ejs = require('ejs')
 const serverless = require('serverless-http');
 const app = express();
-const fs = require ('fs')
 const path = require('path')
-
-
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-ejs.renderFile("./views/index.ejs").then( html =>{
-    fs.writeFile('./views/index.html', html, function (err) {
-        if (err) throw err;
-    });
-})
 app.use('/',(req, res)=>{
     res.sendFile(path.join(__dirname, "../views/index.html"))
-    // res.sendFile(path.join(__dirname, "../views/index.ejs"))
     // res.render('index');
 })
-
-//debug
-// app.get("/*", (req, res) =>{
-//   res.sendFile(path.join(__dirname, "../views/index.ejs"))
-// })
 
 //serverless
 const handler = serverless(app);
 module.exports.handler = async (event, context) => {
     return await handler(event, context);
 };
-
-// app.listen(3000, () => {
-//     console.log("Listening on port 3000")
-// });
